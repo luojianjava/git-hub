cf22c966a1b28d2ccb3511a6002e4950884d02da
luo_jian <luo_jian@dingtian.com.cn>
2020-03-04 22:48:15 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+/**
+ * 类名称：Test
+ * 类描述：
+ * 创建人：luoj
+ * 创建时间：2020/3/4 22:47
+ * 修改人：dorequest
+ * 修改时间：2020/3/4 22:47
+ * 修改备注：
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
